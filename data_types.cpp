fea1221d86dd8ffd119f44f27e0b02f39a563c3f
Meet Jain <business.id2305@gmail.com>
2026-06-04 08:05:34 +0530

    progress

---
+++ data_types.cpp
@@ -0,0 +1,19 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main()
+{
+int a = '3';
+float b = 53.33534;
+char c = 'v';
+long d = 3393235;
+long long e = 49358939389583;
+float f = 5;
+string g;
+string g2;
+string str; 
+getline(cin, str);
+cin >> g >> g2;
+cout << "the value of a is" << a << "the value of b is " << b << "the value of c is "<< c << "the value of d is " << d;
+cout << "the value of e is " << e << "the value of f is "<< f <<"the value of g is " << g << "and " << g2 << "and " << str;
+return 0;
+}

c0c503a9d6d1bd03d704e3ae411d9b4e3dcbc9de
Meet Jain <business.id2305@gmail.com>
2026-06-23 20:47:01 +0530

    Longest Substring Without Repeating Characters

---
+++ longest_subs.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int longestUniqueSubstr(string &s){
+    int n = s.size();
+    int res = 0;
+
+    for (int i = 0; i < n; i++){
+
+        vector<bool> vis(26, false);
+
+        for (int j = i; j < n; j++){
+
+            if (vis[s[j] - 'a'] == true)
+                break;
+
+            else
+            {
+                res = max(res, j - i + 1);
+                vis[s[j] - 'a'] = true;
+            }
+        }
+    }
+    return res;
+}
+
+int main(){
+    string s = "geeksforgeeks";
+    cout << longestUniqueSubstr(s);
+    return 0;
+}
